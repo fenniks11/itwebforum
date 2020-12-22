0c1651c49b02793e8d8a0eb6d0f836a599400ddc
dnm13 <dn_m13@yahoo.com>
2020-12-22 05:53:50 +0200

    email register fix, login & register snackbar addition

--- src/app/home/login/login.component.ts
+++ src/app/home/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, NgModule } from '@angular/core';
 import { HttpClient } from "@angular/common/http";
 import { Router, ActivatedRoute, ParamMap } from '@angular/router';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'login',
@@ -8,22 +9,37 @@ import { Router, ActivatedRoute, ParamMap } from '@angular/router';
   //   styleUrls: ["login.css"]
 })
 export class Login {
-  constructor(private router: Router, private route: ActivatedRoute, private http: HttpClient) { };
+  constructor(private router: Router, private route: ActivatedRoute, private http: HttpClient, private snackBar: MatSnackBar) { };
   wrong = false;
   logged_in = !sessionStorage.getItem("_id") ? false : true;
   email = "";
   password = "";
   oncomplete = this.route.snapshot.paramMap.get("oncomplete");
+
   ngOnInit() {
     if (this.logged_in) this.router.navigate(["/"])
   }
+
+  errorLogin(reason) {
+    if (reason == 404) {
+      this.snackBar.open(`Silahkan check kembali email kamu`, null, { duration: 5000 });
+      this.email = ""
+      this.password = ""
+    }
+    else if (reason == 401) {
+      this.snackBar.open(`Password salah!`, null, { duration: 5000 });
+      this.password = ""
+    }
+  }
+
   async login() {
     var res = await this.http.post("http://localhost:3000/api/user/login", { email: this.email, password: this.password }).toPromise() as any;
-    if (!res.success) { alert(res.reason /*404 = not found, 401 = unauthorized (wrong pass) */); alert(this.password); this.email = this.email = this.password = ""; return }
+    if (!res.success) return this.errorLogin(res.reason /*404 = not found, 401 = unauthorized (wrong pass) */);
     sessionStorage.setItem("_id", res.id)
-    if(this.oncomplete) this.router.navigateByUrl(this.oncomplete)
+    if (this.oncomplete) this.router.navigateByUrl(this.oncomplete)
     else window.location.reload()
   }
+
   register() {
     this.router.navigate(['register'])
   }
--- src/app/home/register/register.component.ts
+++ src/app/home/register/register.component.ts
@@ -15,18 +15,36 @@ export class Register {
   username = "";
   logged_in = !sessionStorage.getItem("_id") ? false : true;
   password = "";
+
   ngOnInit() {
     if (this.logged_in) this.router.navigate(["/"])
   }
+
+  validateEmail(email) {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+  }
+
   login() {
     this.router.navigate(['login'])
   }
+
+
   async register() {
+
+    if (!await this.validateEmail(this.email)) {
+      this.snackBar.open(`Format email salah!`, null, { duration: 5000 });
+      this.email = "";
+      return;
+    }
 
     var body = { email: this.email, username: this.username, password: this.password }
     var res2 = await this.http.post("http://localhost:3000/api/user/register", body).toPromise() as any;
 
-    if(!res2.success) return alert(`${res2.reason} sudah terpakai, silahkan coba ${res2.reason} yang lain`)
+    if (!res2.success) {
+      this.snackBar.open(`${res2.reason} sudah terpakai, silahkan coba ${res2.reason} yang lain`, null, { duration: 5000 })
+      return;
+    }
 
     await sessionStorage.setItem("_id", res2.id)
     this.snackBar.open(`Memproses...`, null, { duration: 2000 })
